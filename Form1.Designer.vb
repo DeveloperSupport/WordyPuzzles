f9dda794c2f3cec3e2a381007c31a320e9202481
Andy Flagg <developersupport@mountaincomputers.org>
2024-09-29 10:30:42 -0700

    WordyPuzzles idea
    its

---
+++ Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.MyStatusLine = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BtnRandomWords = New System.Windows.Forms.Button()
+        Me.BtnGames = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(775, 399)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(148, 36)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MyStatusLine
+        '
+        Me.MyStatusLine.AutoSize = True
+        Me.MyStatusLine.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MyStatusLine.Location = New System.Drawing.Point(12, 410)
+        Me.MyStatusLine.Name = "MyStatusLine"
+        Me.MyStatusLine.Size = New System.Drawing.Size(79, 25)
+        Me.MyStatusLine.TabIndex = 1
+        Me.MyStatusLine.Text = "Status"
+        Me.MyStatusLine.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 18
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(911, 382)
+        Me.ListBox1.TabIndex = 2
+        '
+        'BtnRandomWords
+        '
+        Me.BtnRandomWords.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRandomWords.Location = New System.Drawing.Point(621, 399)
+        Me.BtnRandomWords.Name = "BtnRandomWords"
+        Me.BtnRandomWords.Size = New System.Drawing.Size(148, 36)
+        Me.BtnRandomWords.TabIndex = 3
+        Me.BtnRandomWords.Text = "&Random Words"
+        Me.BtnRandomWords.UseVisualStyleBackColor = True
+        '
+        'BtnGames
+        '
+        Me.BtnGames.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnGames.Location = New System.Drawing.Point(467, 399)
+        Me.BtnGames.Name = "BtnGames"
+        Me.BtnGames.Size = New System.Drawing.Size(148, 36)
+        Me.BtnGames.TabIndex = 4
+        Me.BtnGames.Text = "&Games"
+        Me.BtnGames.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(935, 450)
+        Me.Controls.Add(Me.BtnGames)
+        Me.Controls.Add(Me.BtnRandomWords)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.MyStatusLine)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "Form1"
+        Me.Text = "WordyPuzzles"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents MyStatusLine As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BtnRandomWords As Button
+    Friend WithEvents BtnGames As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports System.Data.SQLite
+Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call mySQLiteReadDB()
+    End Sub
+    Private Function MyDictionaryExists()
+        MyDictionaryExists = False
+        If My.Computer.FileSystem.FileExists("en-dictionary.db") Then
+            MyStatusLine.Text = "Dictionary db file exists."
+            'Debug.Print("Dictionary db file exists.")
+            ListBox1.Items.Add("Dictionary db file exists.")
+            MyDictionaryExists = True
+        Else
+            MyStatusLine.Text = "Dictionary db file does not exist."
+            'Debug.Print("Dictionary db file does not exist.")
+            ListBox1.Items.Add("Dictionary db file does not exist.")
+        End If
+    End Function
+
+    Private Sub mySQLiteReadDB()
+        Try
+            If MyDictionaryExists() Then
+                Dim myconnectionString = "Data Source=" & Application.StartupPath & "\en-dictionary.db" & ";Version=3; FailIfMissing=True;"
+                Dim myConnection As SQLiteConnection = New SQLiteConnection()
+                Dim myCommand As New SQLite.SQLiteCommand()
+                Dim myData As SQLite.SQLiteDataReader
+
+                myConnection.ConnectionString = myconnectionString
+                myConnection.Open()
+                ' execute queries, etc
+
+                'get max row count.
+                myCommand = myConnection.CreateCommand
+                myCommand.CommandText = "SELECT count(*) as [maxcount] FROM e"
+                myData = myCommand.ExecuteReader()
+                myData.Read()
+
+                'get the max records in the english db
+                Dim maxrec
+                maxrec = 0
+                If myData.HasRows Then
+                    maxrec = myData(0)
+                    ListBox1.Items.Add("max record count: " & maxrec)
+                    'Debug.Print("max record count: " & maxrec)
+                    MyStatusLine.Text = "Dictionary db file exists." & maxrec & " entries."
+                End If
+
+                'just get me a random starting point st from 1 to maxrecords
+                Dim st
+                Dim random As New Random()
+                random.Next()
+                st = random.Next(1, maxrec)
+
+                'check for the over, you won't run into the under
+                'this means the st is ver close to maxrec
+                If maxrec - st < 15 Then
+                    'set the starting point (st) to what it was -25
+                    st -= 25
+                End If
+
+                's now is a random start value in the record set
+
+                myCommand = myConnection.CreateCommand
+                'myCommand.CommandText = "SELECT * FROM e LIMIT 15 "
+                myCommand.CommandText = "SELECT * FROM e where field1 >= " & st & " LIMIT 15"
+                myData = myCommand.ExecuteReader()
+                myData.Read()
+                If myData.HasRows Then
+                    ListBox1.Items.Add("Has rows. Reading 15 random records starting at row " & st)
+                    Dim mycount
+                    mycount = 1
+                    Do While (myData.Read())
+                        ListBox1.Items.Add(mycount & ") " & myData(1) & "," & myData(2) & ", " & myData(3))
+                        'Debug.Print(mycount & ") " & myData(1) & ", " & myData(2) & ", " & myData(3))
+                        mycount = mycount + 1
+                    Loop
+                End If
+
+                myConnection.Close()
+            End If
+
+        Catch
+            'nothing here to do
+        Finally
+            'nothing here to do
+        End Try
+
+    End Sub
+
+    Private Sub BtnRandomWords_Click(sender As Object, e As EventArgs) Handles BtnRandomWords.Click
+        ListBox1.Items.Clear()
+        Call mySQLiteReadDB()
+    End Sub
+
+    Private Sub BtnGames_Click(sender As Object, e As EventArgs) Handles BtnGames.Click
+        Form2.Show()
+    End Sub
+
+
+End Class
